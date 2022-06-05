fec2951011e2eba01a6b4772832aa57cb147042e
Pavlin Zhelezchev <battlebg12@gmail.com>
2022-06-05 13:13:43 +0300

    removed the port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ import { graphql } from 'graphql';
 dotenv.config();
 
 const graphQlPath = process.env.GRAPHQL_PATH;
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const dbUrl = process.env.MONGODB_URL
 
 mongoose.connect(dbUrl, {
